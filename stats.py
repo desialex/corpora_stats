6ee07ae777e82b2afeadfbfdbd4d26870dba98fc
Francois Lareau <francois.lareau@umontreal.ca>
2019-04-10 11:03:16 -0400

    Partial merge of functions rel_stats() and tree_stats()

--- stats.py
+++ stats.py
@@ -5,32 +5,23 @@
 from conllu import parse
 from conllu import parse_tree
 import numpy as np
-import collections
+import collections.abc
+from pprint import pprint
 
 # ---- Project libraries -------------------------------------------------------
 from conll import load_conll, parse_conll, parse_tree_conll
 
 
-def children_per_branch(forest):
-	dictionary = dict()  # defines the dictionary var
-	for root in forest:
-		climb_tree(root, dictionary)
-	return dictionary
-
-
-def climb_tree(root, dictionary):
-	count_children(root, dictionary)
-	if root.children:
-		for child in root.children:
-			climb_tree(child, dictionary)
-
-def count_children(root, dictionary):
-	dictionary.setdefault(root.token['deprel'], dict())
-	dictionary[root.token['deprel']].setdefault(str(len(root.children)), 0)
-	dictionary[root.token['deprel']][str(len(root.children))] += 1
-	
 def tree_stats(tree, root_distance=0):
 	stats = {}
+	stats['rels'] = {tree.token['deprel']  : {'branching' : {len(tree.children) : 1}, 
+																						'count' : 1,
+																						'left' : 0,
+																						'right' : 0}}
+	stats['postags'] = {tree.token['upostag'] : {'branching' : {len(tree.children) : 1},
+																							 'count' : 1,
+																							 'left' : 0,
+																							 'right' : 0}}
 	children_stats = [tree_stats(child, root_distance+1) for child in tree.children]
 	stats['weight'] = sum([d['weight'] for d in children_stats]) + 1
 	stats['depth'] = max([d['depth'] for d in children_stats], default=-1) + 1
@@ -42,6 +33,8 @@ def tree_stats(tree, root_distance=0):
 	stats['hd'] = root_distance
 	stats['ddsum'] = sum([d['ddsum'] for d in children_stats]) + stats['dd']
 	stats['hdsum'] = sum([d['hdsum'] for d in children_stats]) + stats['hd']
+	stats['rels'] = merge_dicts([stats['rels']] + [d['rels'] for d in children_stats])
+	stats['postags'] = merge_dicts([stats['postags']] + [d['postags'] for d in children_stats])
 	if stats['weight'] > 1:
 		stats['mdd'] = stats['ddsum'] / (stats['weight'] - 1)
 		stats['mhd'] = stats['hdsum'] / (stats['weight'] - 1)	
@@ -62,54 +55,48 @@ def dict_merge(dct, merge_dct):
 	"""
 	for k, v in merge_dct.items():
 		if k in dct:
-			if isinstance(dct[k], dict) and isinstance(merge_dct[k], collections.Mapping):
+			if isinstance(dct[k], dict) and isinstance(merge_dct[k], collections.abc.Mapping):
 				dict_merge(dct[k], merge_dct[k])
 			else:
 				dct[k] += merge_dct[k] # assumes the values are numerical
 		else:
 			dct[k] = merge_dct[k]
 
+def merge_dicts(dicts):
+	"""Merges a list of dictionaries"""
+	merged = {}
+	for d in dicts:
+		dict_merge(merged, d)
+	return merged
 
-def rel_stats(tree):
-	if tree.children:
-		rels = {}
-		rels.update({tree.token['deprel']  : {'branching' : {len(tree.children) : 1}, 
-																					'count' : 1,
-																					'lr_branching' : {'left' : 0, 'right' : 0}}})
-		rels.update({tree.token['upostag'] : {'branching' : {len(tree.children) : 1},
-																					'count' : 1,
-																					'lr_branching' : {'left' : 0, 'right' : 0}}})
-		for child in tree.children: 
-			dict_merge(rels, rel_stats(child))
-			rels[child.token['deprel']].setdefault('gPOS:dPOS', dict())
-			rels[child.token['deprel']]['gPOS:dPOS'].setdefault(str(tree.token['upostag'])+':'+
-																str(child.token['upostag']), 1)
-			if child.token['id'] < tree.token['id']:
-				rels[tree.token['deprel']]['lr_branching']['left'] += 1
-				rels[tree.token['upostag']]['lr_branching']['left'] += 1
-			else:
-				rels[tree.token['deprel']]['lr_branching']['right'] += 1
-				rels[tree.token['upostag']]['lr_branching']['right'] += 1
-		return rels
-	else:
-		return {tree.token['deprel']:{'branching' : {0 : 1}, 'count' : 1}, 
-			   tree.token['upostag']:{'branching' : {0 : 1}, 'count' : 1}}
+# def rel_stats(tree):
+# 	if tree.children:
+# 		rels = {}
+# 		rels.update({tree.token['deprel']  : {'branching' : {len(tree.children) : 1}, 
+# 																					'count' : 1,
+# 																					'lr_branching' : {'left' : 0, 'right' : 0}}})
+# 		rels.update({tree.token['upostag'] : {'branching' : {len(tree.children) : 1},
+# 																					'count' : 1,
+# 																					'lr_branching' : {'left' : 0, 'right' : 0}}})
+# 		for child in tree.children: 
+# 			dict_merge(rels, rel_stats(child))
+# 			rels[child.token['deprel']].setdefault('gPOS:dPOS', dict())
+# 			rels[child.token['deprel']]['gPOS:dPOS'].setdefault(str(tree.token['upostag'])+':'+
+# 																str(child.token['upostag']), 1)
+# 			if child.token['id'] < tree.token['id']:
+# 				rels[tree.token['deprel']]['lr_branching']['left'] += 1
+# 				rels[tree.token['upostag']]['lr_branching']['left'] += 1
+# 			else:
+# 				rels[tree.token['deprel']]['lr_branching']['right'] += 1
+# 				rels[tree.token['upostag']]['lr_branching']['right'] += 1
+# 		return rels
+# 	else:
+# 		return {tree.token['deprel']:{'branching' : {0 : 1}, 'count' : 1}, 
+# 			   tree.token['upostag']:{'branching' : {0 : 1}, 'count' : 1}}
 
 
 if __name__ == "__main__":
-	path = "data/fr_ftb-ud-dev.conllu"
+	path = "data/test.conllu"
 	trees = parse_tree_conll(path)
 	stats = [tree_stats(tree) for tree in trees]
-	print(stats[0])
-	_ = [print(r) for r in rel_stats(trees[0]).items()]
-#	 print(np.array([s['mdd'] for s in stats]).mean())
-#	 print(max([s['mdd'] for s in stats]))
-#	 mw = np.array([d['weight'] for d in stats]).mean()
-#	 md = np.array([d['depth'] for d in stats]).mean()
-#	 print(mw, md)
-#	 children = children_per_branch(parse_tree_conll(path))
-#	 print(children.keys())
-#	 print(children['root'])
-#	 sents = parse_tree_conll(path)
-#	 print("{'relation': {number of immidiate dependents : number of occurences}}")
-#	 print(rel_stats(sents[1]))
+	pprint(stats[0])
