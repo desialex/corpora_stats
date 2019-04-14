9fb28671d9c24fb16abbc4c5fc530d36c494fadc
desialex <3835505+desialex@users.noreply.github.com>
2019-04-14 19:43:58 -0400

    Added corpus_stats()

--- stats.py
+++ stats.py
@@ -6,7 +6,9 @@ from conllu import parse
 from conllu import parse_tree
 import numpy as np
 import collections.abc
+from collections import OrderedDict as od
 from pprint import pprint
+from scipy import stats
 
 # ---- Project libraries -------------------------------------------------------
 from conll import load_conll, parse_conll, parse_tree_conll
@@ -19,10 +21,10 @@ def tree_stats(tree, root_distance=0, gov_pos='ROOT'):
     stats['root_id'] = tree.token['id']
     rel = tree.token['deprel']
     pos = tree.token['upostag']
-    stats['rels'] = {rel  : {'branches' : {len(tree.children) : 1}, 
-                                                     'count' : 1}}
-    stats['postags'] = {pos : {'branches' : {len(tree.children) : 1},
-                                                         'count' : 1}}
+    stats['rels'] = {rel: {'branches': {len(tree.children): 1}, 
+                           'count': 1}}
+    stats['postags'] = {pos: {'branches': {len(tree.children): 1}, 
+                              'count': 1}}
 
     # Get stats for all children (children_stats is a list of stat dictionaries)
     children_stats = [tree_stats(child, root_distance+1, tree.token['upostag']) for child in tree.children]
@@ -89,8 +91,82 @@ def merge_dicts(dicts):
         dict_merge(merged, d)
     return merged
 
+
+def corpus_stats(trees):
+    merged = merge_dicts(tree_stats(tree) for tree in trees)
+    
+    items = sum([rel['count'] for rel in merged['rels'].values()]) # items = nodes = edges
+    rels_branch_patns = sum([len(rel['branches']) for rel in merged['rels'].values()])
+    pos_branch_patns = sum([len(pos['branches']) for pos in merged['postags'].values()])
+    items_rdist = [v / items for v in od(sorted(merge_dicts(rel['branches'] for rel in merged['rels'].values()).items())).values()]
+    
+    # Create a new dictionary
+    corpus = {}
+    corpus['rels'] = {rel: dict() for rel in merged['rels']}    
+    corpus['postags'] = {pos: dict() for pos in merged['postags']}
+    
+    # Corpus stats
+    corpus['mdd'] = merged['mdd'] / len(trees) # MDD
+    corpus['mhd'] = merged['mhd'] / len(trees) # MHD
+    corpus['depth'] = merged['depth'] / len(trees) # mean depth
+    corpus['weight'] = merged['weight'] / len(trees) # mean weight 
+
+    for pos,dic in merged['postags'].items():
+        # Pos counts
+        corpus['postags'][pos]['r_freq'] = dic['count'] / items
+        corpus['postags'][pos]['avg_freq'] = dic['count'] / len(trees) 
+        
+        # Branch counts
+        corpus['postags'][pos]['r_branch_patns'] = len(dic['branches']) / pos_branch_patns 
+        corpus['postags'][pos]['left'] = dic['left'] / (dic['left'] + dic['right']) if dic['left'] > 0 else 0 
+        corpus['postags'][pos]['right'] = dic['right'] / (dic['left'] + dic['right']) if dic['right'] > 0 else 0        
+
+        # Branch patterns distribution
+        branches_sum = sum([v for v in dic['branches'].values()])
+        branches_dist = [v for v in od(sorted(dic['branches'].items())).values()]
+        branches_rdist = [v / branches_sum for v in od(sorted(dic['branches'].items())).values()]
+        
+        corpus['postags'][pos]['std'] = np.std(branches_rdist) # spread
+        corpus['postags'][pos]['var'] = np.var(branches_rdist) # spread
+        corpus['postags'][pos]['skew'] = stats.skew(branches_dist) # shape
+        corpus['postags'][pos]['mean'] = np.mean(branches_rdist) # location
+        corpus['postags'][pos]['range'] = stats.iqr(branches_rdist) # spread
+        corpus['postags'][pos]['median'] = np.median(branches_rdist) # location
+        corpus['postags'][pos]['kurtosis'] = stats.kurtosis(branches_dist) # shape
+        corpus['postags'][pos]['entropy'] = stats.entropy(branches_rdist) # location
+        corpus['postags'][pos]['anova'] = stats.f_oneway(items_rdist, branches_rdist)[0] # correlation (to remove?)
+
+    for rel,dic in merged['rels'].items():
+        # Rel counts
+        corpus['rels'][rel]['r_freq'] = dic['count'] / items
+        corpus['rels'][rel]['avg_freq'] = dic['count'] / len(trees)
+        
+        # Branch counts
+        corpus['rels'][rel]['r_branch_patns'] = len(dic['branches']) / rels_branch_patns
+        corpus['rels'][rel]['left'] = dic['left'] / (dic['left'] + dic['right']) if dic['left'] > 0 else 0 
+        corpus['rels'][rel]['right'] = dic['right'] / (dic['left'] + dic['right']) if dic['right'] > 0 else 0
+       
+        # Branch patterns distribution
+        branches_sum = sum([v for v in dic['branches'].values()])
+        branches_dist = [v for v in od(sorted(dic['branches'].items())).values()]
+        branches_rdist = [v / branches_sum for v in od(sorted(dic['branches'].items())).values()]
+        
+        corpus['rels'][rel]['std'] = np.std(branches_rdist) # spread
+        corpus['rels'][rel]['var'] = np.var(branches_rdist) # spread
+        corpus['rels'][rel]['skew'] = stats.skew(branches_dist) # shape
+        corpus['rels'][rel]['mean'] = np.mean(branches_rdist) # location
+        corpus['rels'][rel]['range'] = stats.iqr(branches_rdist) # spread
+        corpus['rels'][rel]['median'] = np.median(branches_rdist) # location
+        corpus['rels'][rel]['kurtosis'] = stats.kurtosis(branches_dist) # shape
+        corpus['rels'][rel]['entropy'] = stats.entropy(branches_rdist) # location
+        corpus['rels'][rel]['anova'] = stats.f_oneway(items_rdist, branches_rdist)[0] # correlation (to remove?)
+
+    return corpus
+
+
 if __name__ == "__main__":
     path = "data/fr_ftb-ud-dev.conllu"
     trees = parse_tree_conll(path)
-    stats = [tree_stats(tree) for tree in trees]
-    pprint(stats[0])
+#     stats = [tree_stats(tree) for tree in trees]
+#     pprint(stats[0])
+    pprint(corpus_stats(trees[:100]))
