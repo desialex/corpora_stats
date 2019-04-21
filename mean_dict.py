992468cd4c064f3765d2489910a602f60a133b4c
François Lareau <francois.lareau@umontreal.ca>
2019-04-21 16:47:31 -0400

    mean_dict and normalize_dicts

--- mean_dict.py
+++ mean_dict.py
@@ -3,22 +3,21 @@ from pprint import pprint
 
 def keyset(dicts):
     """
-    Returns the list of all keys and value types
-    in a number of dictionaries
+    Returns the list of all keys and value types in a list of dictionaries
     """
     return sorted(set([(k,type(d[k])) for d in dicts for k in d.keys()]))
 
 def normalize_keys(d, keys):
     for k,t in keys:
         if k not in d.keys():
-            if str(t) == "<class 'numpy.ndarray'>":
-                d[k] = np.array([])
-            else:
-                d[k] = t()
+            d[k] = t()
     return d
 
 def mean_dict(dicts):
-    """Takes a list of dicts and returns the average dict"""
+    """
+    Takes a list of dicts and returns a dict with mean values
+    Assuming values are int/float or dict
+    """
     keys = keyset(dicts) # list of keys and types
     dicts = [normalize_keys(d, keys) for d in dicts] # make sure we have the same keys
     mean = {}
@@ -26,13 +25,32 @@ def mean_dict(dicts):
         values = [d[k] for d in dicts]
         if str(t) == "<class 'dict'>":
             mean[k] = mean_dict(values)
-        elif str(t) == "<class 'numpy.ndarray'>":
-            mean[k] = np.mean(np.concatenate(values))
         else:
-            mean[k] = np.mean(np.array(values))
+            mean[k] = np.mean(values)
     return mean
 
-d1 = {'a':1, 'b':np.array([1,1]), 'c':{'d':1}, 'f':1, 'h':np.array([0])}
-d2 = {'a':3, 'b':np.array([4,4,0]), 'c':{'e':1}, 'g':{}}
-dd = [d1,d2]
+def normalize_dicts(dicts):
+    """Normalize the values across a list of dictionaries (offset and scale)"""
+    keys = keyset(dicts) # list of keys and types
+    dicts = [normalize_keys(d, keys) for d in dicts] # make sure we have the same keys
+    for k,t in keys:
+        if str(t) == "<class 'dict'>":
+            values = [d[k] for d in dicts]
+            values = normalize_dicts(values)
+        else:
+            values = np.array([d[k] for d in dicts])
+            if values.max() - values.min() != 0:
+                values = (values - values.min()) / (values.max() - values.min())
+        for d,v in zip(dicts, values):
+            d[k] = v
+    return dicts
+
+
+d1 = {'a':1, 'b':1, 'c':{'d':4, 'i':{'j':4}}, 'f':4, 'h':0}
+d2 = {'a':3, 'b':3, 'c':{'e':4}, 'g':{}}
+d3 = {'a':5, 'c':{'e':2}}
+dd = [d1,d2,d3]
+
 pprint(mean_dict(dd))
+print()
+pprint(normalize_dicts(dd))
